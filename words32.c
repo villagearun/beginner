781f01d1f31f04def0c423db992992360ea23db3
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-10 12:25:34 +0000

    Create words32.c

---
+++ words32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int count=0,i;
+char a[50];
+scanf("%[^\n]s",s);
+for(i=0;a[i]!='\0';i++)
+{
+ if(a[i]==' ')
+ count++;
+ }
+ printf("\n Number of words:%d",count);
+ return 0;
+}
