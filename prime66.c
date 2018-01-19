e5db7f04719e45e57d67647bf771b9aa9e5c08fd
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-19 19:04:00 +0530

    Create prime66.c

---
+++ prime66.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+ if(n%i==0)
+ {
+ count++;
+ break;
+ }
+}
+if(count==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+return 0;
+}
