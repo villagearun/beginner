3e65bfa39ec6292a7bcb1fc4381d480660dd305f
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-19 08:12:35 +0000

    Create power50.c

---
+++ power50.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+bool isPowerOfTwo(int n)
+{
+return (ceil(log2(n)) == floor(log2(n)));
+}
+int main()
+{
+isPowerOfTwo(31)? printf("Yes\n"): printf("No\n");
+isPowerOfTwo(64)? printf("Yes\n"): printf("No\n");
+return 0;
+}
