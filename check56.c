290b718febcb7e072ca3b8e8a4b0197706fb06d4
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-21 11:37:07 +0530

    Create check56.c

---
+++ check56.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[50];
+scanf("%s",a);
+for(i=0;s[i]!='\0';i++)
+{
+ if(s[i]>='0'&&s[i]<='9')
+ digit++;
+ if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+ num++;
+}
+if(digit>=&&num>0)
+printf("\nYES");
+return 0;
+}
