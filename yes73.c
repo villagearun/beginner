84bf4ff44ddd45a998815686b6c7b184a0e4511b
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-21 09:40:50 +0530

    Create yes73.c

---
+++ yes73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,l,r;
+printf("\n Enter n,l,r:");
+scanf("%d %d %d",&n,&l,&r);
+if(n>=l&&n<=r)
+{
+printf("\nYES");
+}
+else
+{
+printf("\nNO");
+}
+return 0;
+}
