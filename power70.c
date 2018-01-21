e779c9f2e5a4af8b04b74bf83bbab1bf696d61bb
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-21 11:49:52 +0530

    Create power70.c

---
+++ power70.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,c=0,x=1,p=2,i;
+scanf("%d",&n);
+while(n!=1)
+{
+n=n/2;
+c++;
+}
+for(i=0;i<c+1;i++)
+{
+x=x*p;
+}
+printf("The nearest greater power of 2 is:%d",x);
+return 0;
+}
