0c23f81db97caa8e4f639e862170a4910f3a8596
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-23 15:57:39 +0530

    Create small115.c

---
+++ small115.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<algorithm>
+using namespace std;
+int small(int arr[], int n, int k)
+{
+	sort(arr, arr+n);
+  return arr[k-1];
+}
+int main()
+{
+	int a[] = {12, 3, 5, 7, 19};
+	int n = sizeof(arr)/sizeof(arr[0]), k = 2;
+	cout << "K'th smallest element is " << small(a, n, k);
+	return 0;
+}
