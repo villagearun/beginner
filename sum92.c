8f237b88f6d98125e18729a51ca5b6c9e3b670a6
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-01-25 16:07:46 +0530

    Create sum92.c

---
+++ sum92.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,sum=0,n,j;
+scanf(“%d”,&n);
+for(i=0;i<n;i++)
+{
+scanf(“%d”,&a[i]);
+}
+for(j=0;j<n;j++)
+{
+Sum=sum+a[j];
+}
+Printf(“sum=%d”,sum);
+Return 0;
+}
