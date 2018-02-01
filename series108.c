243ba45db8a0e6cff511f659a71bfbe982b1cb35
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-02-01 08:40:40 +0000

    Create series108.c

---
+++ series108.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a,n,d;
+float sum;
+printf("enter the values of a,n,d");
+scanf("%d%d%d",&a,&n,&d);
+sum=a+(a+(n-1)d);
+printf("sum of AP series is %f",sum);
+}
